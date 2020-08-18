d74c8baf8683da9d42108752ba45300f4e913792
Nguyen Thanh Phuc <ntphuc98it@gmail.com>
2020-08-18 13:06:11 +0700

    Add migration

---
+++ database/migrations/2020_08_18_054408_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 225);
+            $table->text('description');
+            $table->integer('cost');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->boolean('visiable');
+            $table->string('slug', 225);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
